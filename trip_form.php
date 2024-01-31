f7db2b6dd9c1b2c3831d452b47f4cad0c9c2e557
Navneet Singh <navneetsingh191302108@gmail.com>
2024-01-31 14:19:52 +0530

    commit

--- trip_form.php
+++ trip_form.php
@@ -1,10 +1,47 @@
+<?php    
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    
+    
+    $con = mysqli_connect($server , $username , $password);
+    
+    if(!$con){
+        die("connection to this database failed due to " . mysqli_connect_error());
+    }
+    else{
+        // echo "Success connection to db";
+    }
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $sql = "INSERT INTO `trip`.`data` (`name`, `email`, `phone`, `dt`) VALUES ('$name', '$email', '$phone', current_timestamp())";
+    
+    // echo $sql;
+    
+    
+    if($con -> query($sql) == true){
+        // echo "Successfully inserted";
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+    
+    $con->close();
+    
+    
+    
+    ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic form</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
 
@@ -15,7 +52,7 @@
 
 <body>
 
-    
+
 
 
 
@@ -23,50 +60,41 @@
         <h3 class="text-center">Welcome to Trip form</h3>
         <p class="my-3 text-center">Enter your details to confirm your participation in the trip & submit the form</p>
 
-        <form action="trip.php" method="post">
+        <form action="" method="post">
             <div class="container">
                 <div class="mb-1">
-                 <label for="name" class="form-label">Name</label>
-                 <input
-                     type="text"
-                     name="name"
-                     id="name"
-                     class="form-control"
-                     placeholder=""
-                     aria-describedby="helpId"
-                 />
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" name="name" id="name" class="form-control" placeholder=""
+                        aria-describedby="helpId" />
                 </div>
                 <div class="mb-1">
-                 <label for="email" class="form-label">Email</label>
-                 <input
-                     type="email"
-                     name="email"
-                     id="email"
-                     class="form-control"
-                     placeholder=""
-                     aria-describedby="helpId"
-                 />
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder=""
+                        aria-describedby="helpId" />
                 </div>
                 <div class="mb-1">
-                 <label for="phone" class="form-label">Phone</label>
-                 <input
-                     type="text"
-                     name="phone"
-                     id="phone"
-                     class="form-control"
-                     placeholder=""
-                     aria-describedby="helpId"
-                 />
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name="phone" id="phone" class="form-control" placeholder=""
+                        aria-describedby="helpId" />
                 </div>
-     
+
                 <div class="text-center">
-                 <button class="btn btn-success" type="submit">Submit</button>
-                 <button class="btn btn-warning" type="reset">Reset</button>
+                    <button class="btn btn-success" type="submit">Submit</button>
+                    <button class="btn btn-warning" type="reset">Reset</button>
                 </div>
-                
-             </div>
+
+            </div>
         </form>
     </div>
+
+    
+
+
+
+
+
+
     <script src="index.js"></script>
 </body>
+
 </html>
